3209a5d7a22b078d5b7c07290b543083c341d733
石冬冬 <913419042@qq.com>
2020-02-25 10:43:29 +0800

    去除对UITableView 和 UICollectionView的支持

--- EmptyViewDemo/DKEmptyDataView/DKEmptyDataSet.swift
+++ EmptyViewDemo/DKEmptyDataView/DKEmptyDataSet.swift
@@ -21,10 +21,6 @@ extension UIView {
         }
         set {
             objc_setAssociatedObject(self, &kConfigureEmptyDataView, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-            UIScrollView.swizzleReloadData
-            if self is UITableView {
-                UIScrollView.swizzleEndUpdates
-            }
         }
     }
     
@@ -59,40 +55,7 @@ extension UIView {
         }
     }
     
-    private var itemsCount: Int {
-       var items = 0
-       
-       // UITableView support
-       if let tableView = self as? UITableView {
-           var sections = 1
-           
-           if let dataSource = tableView.dataSource {
-               if dataSource.responds(to: #selector(UITableViewDataSource.numberOfSections(in:))) {
-                   sections = dataSource.numberOfSections!(in: tableView)
-               }
-               if dataSource.responds(to: #selector(UITableViewDataSource.tableView(_:numberOfRowsInSection:))) {
-                   for i in 0 ..< sections {
-                       items += dataSource.tableView(tableView, numberOfRowsInSection: i)
-                   }
-               }
-           }
-       } else if let collectionView = self as? UICollectionView {
-           var sections = 1
-           
-           if let dataSource = collectionView.dataSource {
-               if dataSource.responds(to: #selector(UICollectionViewDataSource.numberOfSections(in:))) {
-                   sections = dataSource.numberOfSections!(in: collectionView)
-               }
-               if dataSource.responds(to: #selector(UICollectionViewDataSource.collectionView(_:numberOfItemsInSection:))) {
-                   for i in 0 ..< sections {
-                       items += dataSource.collectionView(collectionView, numberOfItemsInSection: i)
-                   }
-               }
-           }
-       }
-       
-       return items
-    }
+    
     
     //MARK: - Reload APIs (Public)
     public func reloadEmptyDataView() {
@@ -104,7 +67,7 @@ extension UIView {
             
             if view.superview == nil {
                 // Send the view all the way to the back, in case a header and/or footer is present, as well as for sectionHeaders or any other content
-                if (self is UITableView) || (self is UICollectionView) {
+                if self is UIScrollView {
                     insertSubview(view, at: 0)
                 } else {
                     addSubview(view)
@@ -135,55 +98,6 @@ extension UIView {
         }
         
     }
-    
-    //MARK: - Method Swizzling
-    @objc private func tableViewSwizzledReloadData() {
-        tableViewSwizzledReloadData()
-        reloadEmptyDataView()
-    }
-    
-    @objc private func tableViewSwizzledEndUpdates() {
-        tableViewSwizzledEndUpdates()
-        reloadEmptyDataView()
-    }
-    
-    @objc private func collectionViewSwizzledReloadData() {
-        collectionViewSwizzledReloadData()
-        reloadEmptyDataView()
-    }
-    
-    private class func swizzleMethod(for aClass: AnyClass, originalSelector: Selector, swizzledSelector: Selector) {
-        let originalMethod = class_getInstanceMethod(aClass, originalSelector)
-        let swizzledMethod = class_getInstanceMethod(aClass, swizzledSelector)
-        
-        let didAddMethod = class_addMethod(aClass, originalSelector, method_getImplementation(swizzledMethod!), method_getTypeEncoding(swizzledMethod!))
-        
-        if didAddMethod {
-            class_replaceMethod(aClass, swizzledSelector, method_getImplementation(originalMethod!), method_getTypeEncoding(originalMethod!))
-        } else {
-            method_exchangeImplementations(originalMethod!, swizzledMethod!)
-        }
-    }
-    
-    private static let swizzleReloadData: () = {
-        let tableViewOriginalSelector = #selector(UITableView.reloadData)
-        let tableViewSwizzledSelector = #selector(UIScrollView.tableViewSwizzledReloadData)
-        
-        swizzleMethod(for: UITableView.self, originalSelector: tableViewOriginalSelector, swizzledSelector: tableViewSwizzledSelector)
-        
-        let collectionViewOriginalSelector = #selector(UICollectionView.reloadData)
-        let collectionViewSwizzledSelector = #selector(UIScrollView.collectionViewSwizzledReloadData)
-        
-        swizzleMethod(for: UICollectionView.self, originalSelector: collectionViewOriginalSelector, swizzledSelector: collectionViewSwizzledSelector)
-    }()
-    
-    private static let swizzleEndUpdates: () = {
-        let originalSelector = #selector(UITableView.endUpdates)
-        let swizzledSelector = #selector(UIScrollView.tableViewSwizzledEndUpdates)
-        
-        swizzleMethod(for: UITableView.self, originalSelector: originalSelector, swizzledSelector: swizzledSelector)
-    }()
-    
 }
 
 
